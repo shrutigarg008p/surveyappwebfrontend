b5e35675854297dd65209f40d8d7dc1b5e258359
jitendrasingh <avani.32trikha@codeaxes.com>
2023-11-22 01:33:52 +0530

    Profile questions.

--- src/Components/ProfileManagement/QuestionsList.tsx
+++ src/Components/ProfileManagement/QuestionsList.tsx
@@ -102,7 +102,7 @@ export class QuestionsList extends Component<any, State> {
                     style={{ zIndex: 1201 }}
                 >
                     <Modal.Header closeButton>
-                        <h5 className="mb-0 mt-1">Questions</h5>
+                        <h5 className="mb-0 mt-1">Create Question</h5>
                         <div
                             className="d-flex justify-content-end w-100 mr-2"
                         >
