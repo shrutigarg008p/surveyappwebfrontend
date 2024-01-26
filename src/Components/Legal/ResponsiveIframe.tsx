e3a2d0732e9aa6ef51d5ee854be06776b6cac746
shivrajan <ssingh79@amgen.com>
2024-01-26 20:15:21 +0530

    New Pages added

---
+++ src/Components/Legal/ResponsiveIframe.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const ResponsiveIframe = ({ url }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getIframeStyle = () => {
+    if (windowWidth >= 1024) { // Desktop
+      return { width: '1000px', height: '600px', border: '0' };
+    } else if (windowWidth >= 768) { // Tablet
+      return { width: '768px', height: '432px', border: '0' };
+    } else { // Mobile
+      return { width: '100%', height: '300px', border: '0' };
+    }
+  };
+
+  return (
+    <iframe 
+      src={url}
+      style={getIframeStyle()}
+      title="Responsive Iframe"
+    ></iframe>
+  );
+};
+
+ResponsiveIframe.propTypes = {
+  url: PropTypes.string.isRequired
+};
+
+export default ResponsiveIframe;
